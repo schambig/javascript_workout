7932dde5ae9775614ae78d76dfec5af46d72fd71
Salomon Chambi <salomonchambi@gmail.com>
2022-11-15 23:33:04 -0500

    feat: create an array and access it

--- JS-understanding_the_basics/app.js
+++ JS-understanding_the_basics/app.js
@@ -1,3 +1,4 @@
+/*
 let greetingText = 'Hi, I\'m Salomón';
 let age = 99;
 alert(greetingText);
@@ -5,3 +6,12 @@ alert(greetingText);
 greetingText = 'Hi I am really Salomón';
 alert(greetingText);
 alert(age);
+*/
+
+let userName = 'Salomon';
+let age = 99;
+// array in js, cero indexed, could hold multiple value types
+let hobbies = ['Reading', 'Inline skating', 'Drawing'];
+
+// access the first element in hobbies array
+alert(hobbies[0]);
