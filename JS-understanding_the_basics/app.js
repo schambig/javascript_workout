6b26d3f49d2ff84fb7e5c3eff81a6ae890a243b5
Salomon Chambi <salomonchambi@gmail.com>
2022-11-16 00:17:29 -0500

    feat: create an object value which holds `label: value` pairs

--- JS-understanding_the_basics/app.js
+++ JS-understanding_the_basics/app.js
@@ -14,6 +14,16 @@ let userName = 'Naomi';
 let age = 30;
 // array in js, cero indexed, could hold multiple value types
 let hobbies = ['Hacking', 'Fixing', 'Hunting'];
+/*
+object: in js are mutable values that store 'label:value' related data
+'label:value' combination is also colled property in js
+we could declare three individual variables and assign the values needed:
+let jobTitle = 'Executive officer';
+let jobPlace = 'Rocinante';
+let jobSalary = 100000;
+but a better aproach is to use objects
+*/
+let job = { title: 'Executive officer', place: 'Rocinante', salary: 100000};
 
 // access the first element in hobbies array
 alert(hobbies[0]);
