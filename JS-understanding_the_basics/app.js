8f189f5fd5ad8402f6460bd9dffde8dc9f0070e1
Salomon Chambi <salomonchambi@gmail.com>
2022-11-16 17:07:11 -0500

    feat: split object and string values in multiple lines

--- JS-understanding_the_basics/app.js
+++ JS-understanding_the_basics/app.js
@@ -16,14 +16,22 @@ let age = 30;
 let hobbies = ['Hacking', 'Fixing', 'Hunting'];
 /*
 object: in js are mutable values that store 'label:value' related data
-'label:value' combination is also colled property in js
+'label:value' combination is also called property in js
 we could declare three individual variables and assign the values needed:
 let jobTitle = 'Executive officer';
 let jobPlace = 'Rocinante';
 let jobSalary = 100000;
 but a better aproach is to use objects
 */
-let job = { title: 'Executive officer', place: 'Rocinante', salary: 100000};
+// use backticks to split string value in multiple lines
+let userLastname = `Nag
+ata`;
+// split object value in  multiple lines
+let job = {
+    title: 'Executive officer',
+    place: 'Rocinante',
+    salary: 100000
+};
 
 // access the first element in hobbies array
 alert(hobbies[0]);
