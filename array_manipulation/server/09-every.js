f903f7a138ab264360d0b8a865da92cba36a893c
Salomon Chambi <salomonchambi@gmail.com>
2023-03-05 23:56:44 -0500

    feat: add definition of every() method

--- array_manipulation/server/09-every.js
+++ array_manipulation/server/09-every.js
@@ -36,3 +36,11 @@ const team = [
 
 const rta3 = team.every(item => item.age < 15);
 console.log('todos menores de 15?:', rta3)
+
+/*
+The every() method in JavaScript is a higher-order array method
+that checks whether all elements in an array pass a certain test implemented by a callback function.
+
+The every() method executes the callback function for each element in the array until the callback returns false for any element.
+If the callback returns true for all elements, the every() method returns true. Otherwise, it returns false.
+*/
