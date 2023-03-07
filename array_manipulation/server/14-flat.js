80aae0b1f36ff3d430eb93085579c635f6add4b3
Salomon Chambi <salomonchambi@gmail.com>
2023-03-06 22:39:33 -0500

    feat: add definition of flat() method

--- array_manipulation/server/14-flat.js
+++ array_manipulation/server/14-flat.js
@@ -18,3 +18,15 @@ console.log('for', newArray);
 // built-in flat() method, indicate the depth of work as an argument of flat
 const rta = matriz.flat(3);
 console.log('flat', rta);
+
+/*
+The flat() method is a built-in method in JavaScript that allows you to flatten a nested array
+by concatenating sub-arrays into a single array.
+
+The flat() method returns a new array that is a flattened version of the original array.
+By default, it flattens the array only one level deep.
+However, you can pass an optional argument to specify the depth level to which the array should be flattened.
+
+The syntax for using the flat() method is as follows:
+    array.flat([depth])
+*/
