280f88ea5e0b95252adc9fa4031e24cf1ede0b7b
Salomon Chambi <salomonchambi@gmail.com>
2023-03-06 22:22:56 -0500

    feat: add definition for concat() method

--- array_manipulation/server/13-concat.js
+++ array_manipulation/server/13-concat.js
@@ -26,3 +26,24 @@ console.log('rta3', rta3);
 // using push(), which is a immutable method, the original array in not changed, cos another array is created
 elements.push(...othersElements);
 console.log('elements', elements);
+
+/*
+In JavaScript, the concat() function is used to combine two or more arrays or strings
+and return a new array or string that contains the concatenated elements.
+
+The syntax for using concat() function is as follows:
+    array1.concat(array2, array3, ..., arrayN)  or
+    string1.concat(string2, string3, ..., stringN)
+
+    const arr1 = [1, 2, 3];
+    const arr2 = [4, 5, 6];
+    const arr3 = [7, 8, 9];
+    const newArray = arr1.concat(arr2, arr3);
+    console.log(newArray); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    const str1 = "Hello";
+    const str2 = "World";
+    const str3 = "!";
+    const newString = str1.concat(" ", str2, str3);
+    console.log(newString); // Output: Hello World!
+*/
