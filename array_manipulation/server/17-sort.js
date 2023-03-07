3afbb3531a3da02437f45a02e3d9835b41acad16
Salomon Chambi <salomonchambi@gmail.com>
2023-03-07 12:10:21 -0500

    feat: add definition for sort() method and extra examples

--- array_manipulation/server/17-sort.js
+++ array_manipulation/server/17-sort.js
@@ -52,3 +52,29 @@ const orders = [
   // the same as ordering an array but considering the properties '.total' in this case
   orders.sort((a,b) => b.total - a.total); 
   console.log(orders);
+
+/*
+The sort() method in JavaScript is used to sort the elements of an array in place.
+It takes an optional comparison function as an argument, which determines how the elements are sorted.
+
+  const numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+  numbers.sort((a, b) => a - b);
+  console.log(numbers); // Output: [1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9]
+
+  const fruits = ['banana', 'apple', 'pear', 'orange'];
+  fruits.sort();
+  console.log(fruits); // Output: ['apple', 'banana', 'orange', 'pear']
+
+Note that the sort() method sorts the elements of the array in place,which means that it modifies the original array.
+If you want to create a sorted copy of an array without modifying the original array,
+you can make a copy of the array first using the slice() method:
+
+  const unsorted = [3, 1, 4, 1, 5];
+  const sorted = unsorted.slice().sort();
+  
+  console.log(sorted); // Output: [1, 1, 3, 4, 5]
+  console.log(unsorted); // Output: [3, 1, 4, 1, 5]
+
+  In this example, we create a copy of the unsorted array using the slice() method,
+  and then sort the copy using the sort() method. The unsorted array is not modified by the sort() method.
+*/
