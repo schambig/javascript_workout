8d06f457e86c5e49eaae46e2976ab2475fd0f170
Salomon Chambi <salomonchambi@gmail.com>
2023-03-06 23:18:22 -0500

    feat: add definition of map() method and extra examples

--- array_manipulation/server/02-map.js
+++ array_manipulation/server/02-map.js
@@ -6,5 +6,24 @@ const newArray = letters.map(item => item + '++');
 //     const element = letters[index];
 //     newArray.push(element + '++');
 // }
-console.log('original', letters);
-console.log('new', newArray);
+console.log('original', letters); // Output: original [ 'a', 'b', 'c' ]
+console.log('new', newArray); // Output: new [ 'a++', 'b++', 'c++' ]
+
+/*
+The map() method is a built-in function in JavaScript that is used to create a new array
+by applying a given function to each element of an existing array.
+The function passed to the map() method takes three arguments:
+    the current element being processed, its index, and the original array.
+
+    const numbers = [1, 2, 3, 4, 5];
+    const squareRoots = numbers.map(function(currentValue, index, array) {
+      return Math.sqrt(currentValue);
+    });
+    console.log(squareRoots); // Output: [1, 1.4142135623730951, 1.7320508075688772, 2, 2.23606797749979]
+
+    const numbers = [1, 2, 3, 4, 5];
+    const squareRoots = numbers.map((currentValue, index, array) => {
+      return Math.sqrt(currentValue);
+    });
+    console.log(squareRoots); // Output: [1, 1.4142135623730951, 1.7320508075688772, 2, 2.23606797749979]
+*/
